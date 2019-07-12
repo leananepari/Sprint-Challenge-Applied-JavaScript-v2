99ebe58756ec4ccccaeef0575c86eeeebb8b3ac6
Leana <leananeparidze.com>
2019-07-12 10:29:12 -0700

    completes carousel functionality

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,9 +17,48 @@
     <div class="right-button"> > </div>
   </div>
 */
-// import img from '../../assets/carousel/mountains'
+Carousel();
 
 function Carousel() {
+  buildCarousel();
+
+  let index = 1; //initial index of an image
+  let leftButton = document.querySelector('.left-button');
+  let rightButton = document.querySelector('.right-button');
+  
+  //Add event listeners to buttons
+  leftButton.addEventListener('click', () => handleLeftClick(event));
+  rightButton.addEventListener('click', () => handleRightClick(event));
+
+  handleLeftClick = (e) => {
+    e.preventDefault();
+    if (index === 1) {
+      document.querySelector(`img[data-img="${index}"]`).classList.remove('selected');
+      index = 4;
+      document.querySelector(`img[data-img="${index}"]`).classList.add('selected');
+    } else {
+      document.querySelector(`img[data-img="${index}"]`).classList.remove('selected');
+      --index;
+      document.querySelector(`img[data-img="${index}"]`).classList.add('selected');
+    }
+  }
+
+  handleRightClick = (e) => {
+    e.preventDefault();
+    if (index === 4) {
+      document.querySelector(`img[data-img="${index}"]`).classList.remove('selected');
+      index = 1;
+      document.querySelector(`img[data-img="${index}"]`).classList.add('selected');
+    } else {
+      document.querySelector(`img[data-img="${index}"]`).classList.remove('selected');
+      ++index;
+      document.querySelector(`img[data-img="${index}"]`).classList.add('selected');
+    }
+  }
+}
+
+
+function buildCarousel() {
   //Create carousel container div
   let carousel = document.createElement('div');
   carousel.className = 'carousel';
@@ -32,15 +71,19 @@ function Carousel() {
   let img1 = document.createElement('img');
   img1.src = "../../assets/carousel/mountains.jpeg";
   img1.className = 'selected';
+  img1.dataset.img = '1';
 
   let img2 = document.createElement('img');
   img2.src = "../../assets/carousel/computer.jpeg";
+  img2.dataset.img = '2';
 
   let img3 = document.createElement('img');
   img3.src = "../../assets/carousel/trees.jpeg";
+  img3.dataset.img = '3';
 
   let img4 = document.createElement('img');
   img4.src = "../../assets/carousel/turntable.jpeg";
+  img4.dataset.img = '4';
 
   let rightBtn = document.createElement('div');
   rightBtn.className = 'right-button';
@@ -56,5 +99,3 @@ function Carousel() {
 
   return document.querySelector('.carousel-container').appendChild(carousel);
 }
-
-Carousel();
